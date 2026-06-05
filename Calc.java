f41219c4d8e8805aa78f88467c6c08fed249cb3d
nyassarodrigue <nyassarodrigue3@gmail.com>
2026-06-05 17:10:43 +0100

    Contrôle de l'âge de vote

--- Calc.java
+++ Calc.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Calc {
-    public static void main ( String [] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println ("Bienvenue sur cette calculatrice java");
-        System.out.print ("Entrer le premier nomnbre ");
+        System.out.println("Bienvenue sur cette calculatrice java");
+        System.out.print("Entrer le premier nomnbre ");
 
         int Nbre = scanner.nextInt();
 
-        System.out.print ("Entrer un second nombre ");
+        System.out.print("Entrer un second nombre ");
         int Nbre2 = scanner.nextInt();
 
-        int nbre3 = Nbre + Nbre2 ;
+        int nbre3 = Nbre + Nbre2;
 
-        System.out.println ("la somme est : " + nbre3);
+        System.out.println("la somme est : " + nbre3);
 
-        scanner.close();       
+        scanner.close(); // fermeture du scanner pour libérer les ressources
 
     }
-    
+
 }
--- ProjetJ1.java
+++ ProjetJ1.java
@@ -1,31 +1,31 @@
 import java.util.Scanner; // importation de la classe Scanner pour lire les entrées de l'utilisateur
 
 public class ProjetJ1 {
-    //  l'utilisateur entre son nom, puis affiche un message de bienvenue personnalisé.
-    public static void main (String [] args) { // point d'entrée du programme
-        System.out.print ("Entrer votre nom : "); // invite l'utilisateur à entrer son nom
-        Scanner scanner = new Scanner (System.in); // création d'un objet Scanner pour lire les entrées au clavier
-        String nom = scanner.nextLine();
-        System.out.println ();
-        System.out.println ("\nBonjour, " + nom + " !" + "\nBienvenue dans le monde de Java.");
+    // l'utilisateur entre son nom, puis affiche un message de bienvenue
+    // personnalisé.
+    public static void main(String[] args) { // point d'entrée du programme
+        System.out.print("Entrer votre nom : "); // invite l'utilisateur à entrer son nom
+        Scanner scanner = new Scanner(System.in); // création d'un objet Scanner pour lire les entrées au clavier
+        String nom = scanner.nextLine(); // recupération du nom de l'utilisateur en tant que chaîne de caractères
+        System.out.println(); // saut de ligne pour améliorer la lisibilité
+        System.out.println("\nBonjour, " + nom + " !" + "\nBienvenue dans le monde de Java.");
 
-        //convertisseur de distance
-        System.out.print ("Entrer une distance en kilomètres : ");
-        double km = scanner.nextDouble(); //recuperation de la distance en km en nombre à virgule flottante
+        // convertisseur de distance
+        System.out.print("Entrer une distance en kilomètres : ");
+        double km = scanner.nextDouble(); // recuperation de la distance en km en nombre à virgule flottante
         double miles = km * 0.621371;
-        System.out.print ("La distance en miles est de : " + miles );
-        System.out.println ();
+        System.out.print("La distance en miles est de : " + miles);
+        System.out.println();
 
-
-        //convertisseur de température
-        System.out.println ("Entrer une température en celsius : ");
+        // convertisseur de température
+        System.out.println("Entrer une température en celsius : ");
         double celsius = scanner.nextDouble();
-        double fahreinheit = (celsius * 9/5) + 32;
-        System.out.println ("La température en fahreinheit est de : " + fahreinheit);
-        System.out.println ();
+        double fahreinheit = (celsius * 9 / 5) + 32;
+        System.out.println("La température en fahreinheit est de : " + fahreinheit);
+        System.out.println();
 
-        //convertisseur de Lb vers kg
-        System.out.println ("Entrer une masse en livres (lb) : ");
+        // convertisseur de Lb vers kg
+        System.out.println("Entrer une masse en livres (lb) : ");
         double lb = scanner.nextDouble();
         double kg = lb * 0.453592;
         System.out.println("La masse en kg est de : " + kg);
@@ -33,6 +33,4 @@ public class ProjetJ1 {
         scanner.close();
     }
 
-
-    
 }
