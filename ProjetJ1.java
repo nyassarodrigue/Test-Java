e40a0b5baee3b069369ed602fd6f3e68d086a034
nyassarodrigue <nyassarodrigue3@gmail.com>
2026-06-05 14:58:29 +0100

    ajout des convertisseurs

--- ProjetJ1.java
+++ ProjetJ1.java
@@ -1,12 +1,34 @@
-import java.util.Scanner;
+import java.util.Scanner; // importation de la classe Scanner pour lire les entrées de l'utilisateur
 
 public class ProjetJ1 {
-    public static void main (String [] args) {
-        System.out.print ("Entrer votre nom :");
-        Scanner scanner = new Scanner (System.in);
+    //  l'utilisateur entre son nom, puis affiche un message de bienvenue personnalisé.
+    public static void main (String [] args) { // point d'entrée du programme
+        System.out.print ("Entrer votre nom : "); // invite l'utilisateur à entrer son nom
+        Scanner scanner = new Scanner (System.in); // création d'un objet Scanner pour lire les entrées au clavier
         String nom = scanner.nextLine();
         System.out.println ();
-        System.out.println ("\nBonjour, " + nom + "!" + "\nBienvenue dans le monde de Java.");
+        System.out.println ("\nBonjour, " + nom + " !" + "\nBienvenue dans le monde de Java.");
+
+        //convertisseur de distance
+        System.out.print ("Entrer une distance en kilomètres : ");
+        double km = scanner.nextDouble(); //recuperation de la distance en km en nombre à virgule flottante
+        double miles = km * 0.621371;
+        System.out.print ("La distance en miles est de : " + miles );
+        System.out.println ();
+
+
+        //convertisseur de température
+        System.out.println ("Entrer une température en celsius : ");
+        double celsius = scanner.nextDouble();
+        double fahreinheit = (celsius * 9/5) + 32;
+        System.out.println ("La température en fahreinheit est de : " + fahreinheit);
+        System.out.println ();
+
+        //convertisseur de Lb vers kg
+        System.out.println ("Entrer une masse en livres (lb) : ");
+        double lb = scanner.nextDouble();
+        double kg = lb * 0.453592;
+        System.out.println("La masse en kg est de : " + kg);
 
         scanner.close();
     }
